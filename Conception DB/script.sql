875484b2c7ee9feaf159889f1ad669cd8db888f3
ranya-tech <amzghar.ranya.solicode@gmail.com>
2026-05-22 13:02:54 +0100

    Conception DB

--- Conception DB/script.sql
+++ Conception DB/script.sql
@@ -5,21 +5,43 @@ CREATE TABLE Client(
     idClient INT AUTO_INCREMENT PRIMARY KEY,
     nom_complet VARCHAR(100) NOT NULL,
     telephone VARCHAR(20) NOT NULL,
-    adresse TEXT NOT NULL
-)
-CREATE TABLE Produit(
-    idProduit INT AUTO_INCREMENT PRIMARY KEY,
-    nom VARCHAR(100),
+    email VARCHAR(100) NOT NULL,
+    mot_de_passe TEXT NOT NULL,
+    role VARCHAR(20) NOT NULL DEFAULT 'client'
+);
+CREATE TABLE Phones(
+    idPhone INT AUTO_INCREMENT PRIMARY KEY,
+    modele VARCHAR(100),
     prix DECIMAL(10,2),
     description TEXT,
     marque VARCHaR(50),
-    stock INT NOT NULL DEFAULT 0
-)
+    stock INT NOT NULL DEFAULT 0,
+    ram VARCHAR(20),
+    stockage VARCHAR(20),  
+    batterie VARCHAR(50),
+    camera VARCHAR(100),
+    image TEXT
+);
 CREATE TABLE Commande(
     idCommande INT AUTO_INCREMENT PRIMARY KEY,
     dateCmd DATETIME DEFAULT CURRENT_TIMESTAMP,
     statut ENUM('En attente', 'Expédiée', 'Livrée') DEFAULT 'En attente',
-    total DECIMAL(10, 2) DEFAULT 0.00,
     idClient INT NOT NULL,
     FOREIGN KEY(idClient) REFERENCES Client(idClient)
-)
+);
+CREATE TABLE Livraison(
+    idLivraison INT AUTO_INCREMENT PRIMARY KEY,
+    adresse TEXT NOT NULL,
+    destinataire TEXT NOT NULL,
+    dateLivraison DATETIME NOT NULL,
+    idCommande INT NOT NULL,
+    FOREIGN KEY(idCommande) REFERENCES Commande(idCommande)
+);
+CREATE TABLE ProduitCmd(
+    idProduitCmd INT AUTO_INCREMENT PRIMARY KEY,
+    idPhones INT NOT NULL, 
+    idCommande INT NOT NULL,
+    quantite INT NOT NULL DEFAULT 1,
+    FOREIGN KEY (idPhones) REFERENCES Phones(idPhones),
+    FOREIGN KEY (idCommande) REFERENCES Commande(idCommande)
+);
