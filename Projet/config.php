51410921baa1df40ae11a1ffc811b3ed3a7443bc
ranya-tech <amzghar.ranya.solicode@gmail.com>
2026-06-08 09:14:11 +0100

    page de connexion au DB

--- Projet/config.php
+++ Projet/config.php
@@ -5,6 +5,7 @@ $username = 'root';
 $password = '';
 try{
     $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $pdo->setAttribute(PDO:: ATTR_ERRMODE, PDO:: ERRMODE_EXCEPTION);
 }catch(PDOxception $a){
     echo "Erreur: " . $a->getMessage();
 }
